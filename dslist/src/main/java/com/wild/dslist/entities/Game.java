e56f7c074938243a872a163c1cf1107b1b547e62
WildBarreto <wildsbsrreto@gmail.com>
2023-07-11 14:40:54 -0300

    Game seed

--- dslist/src/main/java/com/wild/dslist/entities/Game.java
+++ dslist/src/main/java/com/wild/dslist/entities/Game.java
@@ -13,33 +13,40 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_game")
 public class Game {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
-	
+
 	@Column(name ="game_year")
 	private Integer year;
 	private String genre;
-	private String plataform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
+	
 	
 
 	public Game() {
 
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String plataform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
-		
+
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -77,12 +84,20 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlataform() {
-		return plataform;
+	public String getPltforms() {
+		return platforms;
 	}
 
-	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+	public void setPlataform(String plataforms) {
+		this.platforms = plataforms;
+	}
+	
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(String plataform) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
