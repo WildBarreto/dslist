de63c0733ec180c0453277b5e0df888efe3fba91
WildBarreto <wildsbsrreto@gmail.com>
2023-07-12 16:23:32 -0300

    Projections

--- dslist/src/main/java/com/wild/dslist/controllers/GameListController.java
+++ dslist/src/main/java/com/wild/dslist/controllers/GameListController.java
@@ -4,24 +4,34 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wild.dslist.dto.GameListDTO;
+import com.wild.dslist.dto.GameMinDTO;
 import com.wild.dslist.services.GameListService;
+import com.wild.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/lists")
 public class GameListController {
-	
-	
+
 	@Autowired
 	private GameListService gameListService;
-	
-	
+
+	@Autowired
+	private GameService gameService;
+
 	@GetMapping
-	public List<GameListDTO> finAll(){
+	public List<GameListDTO> finAll() {
 		List<GameListDTO> result = gameListService.finAll();
+		return result;
+	}
+
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDTO> findByList(@PathVariable Long listId) {
+		List<GameMinDTO> result = gameService.findByList(listId);
 		return result;
 	}
 }
